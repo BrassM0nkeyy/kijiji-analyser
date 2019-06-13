bee6fce03c1a846cf20c8642f4a52dad5f571035
BrassM0nkeyy <mgrech97@gmail.com>
2019-06-13 03:54:04 -0400

    refactoring to requests 3

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
-from requests import get
-from requests.exceptions import RequestException
+from requests import HTTPSession
+#from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
 
@@ -7,10 +7,9 @@ def get_url(url):
 	"""
 	Returns the HTML/XML content of the URL by making a GET request
 	"""
-	try:
-		resp = get(url, stream=True)
-		return resp.content
+	http = HTTPSession()
 
-	except RequestException as e:
-		log_error('it didnt work {0}'.format(str(e)))
-		return None
+	r = http.request('get', url)
+
+	return r
+
